94119793602f2cf570989fc6377fed5122f08251
asadbekolmasov006-wq <asadbekolmasov006@gmail.com>
2025-12-21 16:06:39 +0500

    Update anide.js

--- anide.js
+++ anide.js
@@ -38,3 +38,21 @@ search.addEventListener("input", () => {
   const v = search.value.toLowerCase();
   show(movies.filter(m => m.title.toLowerCase().includes(v)));
 });
+
+
+
+<img src="/images/naruto.jpg" alt="Naruto" />
+
+
+
+  function Card({ title, imageUrl }) {
+  return (
+    <div style={{ width: '150px', margin: '10px', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 5px rgba(0,0,0,0.3)' }}>
+      <img src={imageUrl} alt={title} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+      <div style={{ padding: '10px', backgroundColor: '#222', color: 'white' }}>
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
+}
+
