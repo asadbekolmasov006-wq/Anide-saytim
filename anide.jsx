b1e279e9c6b25681f715cd52110c480f45735f68
asadbekolmasov006-wq <asadbekolmasov006@gmail.com>
2025-12-21 16:19:33 +0500

    Update anide.jsx

--- anide.jsx
+++ anide.jsx
@@ -1,12 +1,12 @@
 <img src="/images/naruto.jpg" alt="Naruto" />
 
-function Card({ title, imageUrl }) {
-  return (
-    <div style={{ width: '150px', margin: '10px', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 5px rgba(0,0,0,0.3)' }}>
-      <img src={imageUrl} alt={title} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
-      <div style={{ padding: '10px', backgroundColor: '#222', color: 'white' }}>
-        <h3>{title}</h3>
-      </div>
-    </div>
-  );
-}
+// function Card({ title, imageUrl }) {
+//   return (
+//     <div style={{ width: '150px', margin: '10px', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 5px rgba(0,0,0,0.3)' }}>
+//       <img src={imageUrl} alt={title} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+//       <div style={{ padding: '10px', backgroundColor: '#222', color: 'white' }}>
+//         <h3>{title}</h3>
+//       </div>
+//     </div>
+//   );
+// }
